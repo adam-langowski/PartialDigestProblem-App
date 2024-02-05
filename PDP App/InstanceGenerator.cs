e2e9774a1b8f96c588fd5d1bcbf1c43073c28ce4
adam-langowski <72815492+adam-langowski@users.noreply.github.com>
2024-02-05 15:28:32 +0100

    tabu class

--- PDP App/InstanceGenerator.cs
+++ PDP App/InstanceGenerator.cs
@@ -1,129 +1,133 @@
-﻿class PDPInstanceGenerator
+﻿namespace PDP_App
 {
-    private readonly Random random;
-    private readonly List<int> P;    // fragments
-    private readonly List<int> D;    // multiset - tabu input
-    private readonly List<int> Cuts; // map of cuts
-    public bool errorsSuccessful;
-
-    public PDPInstanceGenerator()
+    class PDPInstanceGenerator
     {
-        random = new Random();
-        P = [];
-        D = [];
-        Cuts = [];
-        errorsSuccessful = false;
-    }
+        private readonly Random random;
+        private readonly List<int> P;    // fragments
+        private readonly List<int> D;    // multiset - tabu input
+        private readonly List<int> Cuts; // map of cuts
+        public bool errorsSuccessful;
 
-    /// <summary>
-    /// Generating map P
-    /// </summary>
-    /// <param name="m"></param>
-    /// <param name="dMin"></param>
-    /// <param name="dMax"></param>
-    /// <param name="deletions"></param>
-    /// <param name="substitutions"></param>
-    /// <returns></returns>
-    public List<int> GenerateSolution(int m, int dMin, int dMax)
-    {
-        P.Clear();
-        for (int i = 0; i < m; i++)
+        public PDPInstanceGenerator()
         {
-            int value = random.Next(dMin, dMax + 1);
-            P.Add(value);
+            random = new Random();
+            P = [];
+            D = [];
+            Cuts = [];
+            errorsSuccessful = false;
         }
 
-        return P;
-    }
-
-    /// <summary>
-    /// Generating map - values are cuts sites
-    /// </summary>
-    /// <param name="fragments"></param>
-    /// <returns></returns>
-    public List<int> GenerateCutsMap(List<int> fragments)
-    {
-        Cuts.Clear();
-
-        int sum = 0;
-        Cuts.Add(sum);
-
-        foreach (int fragment in fragments)
+        /// <summary>
+        /// Generating map P
+        /// </summary>
+        /// <param name="m"></param>
+        /// <param name="dMin"></param>
+        /// <param name="dMax"></param>
+        /// <param name="deletions"></param>
+        /// <param name="substitutions"></param>
+        /// <returns></returns>
+        public List<int> GenerateSolution(int m, int dMin, int dMax)
         {
-            sum += fragment;
-            Cuts.Add(sum);
+            P.Clear();
+            for (int i = 0; i < m; i++)
+            {
+                int value = random.Next(dMin, dMax + 1);
+                P.Add(value);
+            }
+
+            return P;
         }
 
-        return Cuts;
-    }
-
-    /// <summary>
-    /// Converting map P to multiset D
-    /// </summary>
-    /// <param name="P"></param>
-    /// <returns></returns>
-    public List<int> GenerateMultiset(List<int> P, int deletions, int substitutions, int minValue, int maxValue)
-    {
-        D.Clear();
-
-        for (int i = 0; i < P.Count; i++)
+        /// <summary>
+        /// Generating map - values are cuts sites
+        /// </summary>
+        /// <param name="fragments"></param>
+        /// <returns></returns>
+        public List<int> GenerateCutsMap(List<int> fragments)
         {
+            Cuts.Clear();
+
             int sum = 0;
-            for (int j = i; j < P.Count; j++)
+            Cuts.Add(sum);
+
+            foreach (int fragment in fragments)
             {
-                sum += P[j];
-                D.Add(sum);
+                sum += fragment;
+                Cuts.Add(sum);
             }
+
+            return Cuts;
         }
 
-        ApplyErrors(D, deletions, substitutions, minValue, maxValue);
+        /// <summary>
+        /// Converting map P to multiset D
+        /// </summary>
+        /// <param name="P"></param>
+        /// <returns></returns>
+        public List<int> GenerateMultiset(List<int> P, int deletions, int substitutions, int minValue, int maxValue)
+        {
+            D.Clear();
 
-        D.Sort();
+            for (int i = 0; i < P.Count; i++)
+            {
+                int sum = 0;
+                for (int j = i; j < P.Count; j++)
+                {
+                    sum += P[j];
+                    D.Add(sum);
+                }
+            }
 
-        return D;
+            ApplyErrors(D, deletions, substitutions, minValue, maxValue);
+
+            D.Sort();
+
+            return D;
+        }
+
+        /// <summary>
+        /// Applying deletions & substitutions
+        /// </summary>
+        /// <param name="P"></param>
+        /// <param name="deletions"></param>
+        /// <param name="substitutions"></param>
+        private void ApplyErrors(List<int> D, int deletions, int substitutions, int minValue, int maxValue)
+        {
+            // deletions
+            if (deletions > D.Count)
+            {
+                MessageBox.Show("Żądana liczba delecji jest większa od liczności multizbioru D", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            for (int i = 0; i < deletions; i++)
+            {
+                if (D.Count > 0)
+                {
+                    int indexToDelete = random.Next(D.Count);
+                    D.RemoveAt(indexToDelete);
+                }
+            }
+
+            // substitutions
+            if (substitutions > D.Count)
+            {
+                MessageBox.Show("Żądana liczba substytucji jest większa od liczności multizbioru D", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            for (int i = 0; i < substitutions; i++)
+            {
+                if (D.Count > 0)
+                {
+                    int indexToReplace = random.Next(D.Count);
+                    int newValue = random.Next(minValue, maxValue);
+                    D[indexToReplace] = newValue;
+                }
+            }
+
+            errorsSuccessful = true;
+        }
     }
 
-    /// <summary>
-    /// Applying deletions & substitutions
-    /// </summary>
-    /// <param name="P"></param>
-    /// <param name="deletions"></param>
-    /// <param name="substitutions"></param>
-    private void ApplyErrors(List<int> D, int deletions, int substitutions, int minValue, int maxValue)
-    {
-        // deletions
-        if (deletions > D.Count)
-        {
-            MessageBox.Show("Żądana liczba delecji jest większa od liczności multizbioru D", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            return;
-        }
-
-        for (int i = 0; i < deletions; i++)
-        {
-            if (D.Count > 0)
-            {
-                int indexToDelete = random.Next(D.Count);
-                D.RemoveAt(indexToDelete);
-            }
-        }
-
-        // substitutions
-        if (substitutions > D.Count)
-        {
-            MessageBox.Show("Żądana liczba substytucji jest większa od liczności multizbioru D", "Błąd", MessageBoxButtons.OK, MessageBoxIcon.Error);
-            return;
-        }
-
-        for (int i = 0; i < substitutions; i++)
-        {
-            if (D.Count > 0)
-            {
-                int indexToReplace = random.Next(D.Count);
-                int newValue = random.Next(minValue, maxValue);
-                D[indexToReplace] = newValue;
-            }
-        }
-
-        errorsSuccessful = true;
-    }
 }
--- PDP App/TabuAlgorithm.cs
+++ PDP App/TabuAlgorithm.cs
@@ -1,8 +1,40 @@
 ﻿namespace PDP_App
 {
-    public class TabuAlgorithm
+    class TabuAlgorithm
     {
-        private List<int> D; // multizbiór wejściowy
+        private List<int> D;        // multizbiór wejściowy
+        private List<int> Solution; // rozwiązanie P
+        private int tabuListSize;
+        private int neighbourhoodPercent;
+        private int restartCount;
+        private int iterations;
+        private readonly Form1 form1 = new();
+        private float objectiveFunction;
+
+        public TabuAlgorithm()
+        {
+            Solution = [];
+            D = form1.GetMultiSet();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void CalculateObjectiveFunction()
+        {
+            objectiveFunction = 0;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="D"></param>
+        public void GenerateInitialSolution(List<int> D)
+        {
+            
+            //Solution
+            //objectiveFunction 
+        } 
 
     }
 }
